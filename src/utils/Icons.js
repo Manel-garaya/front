6d41beb6a5b323f0c7d73d53382bc2fcb56fc082
Rahma <rahmzguir@gamil.com>
2024-01-08 15:15:57 +0100

    first

---
+++ src/utils/Icons.js
@@ -0,0 +1,27 @@
+export const dashboard = <i className="fa-solid fa-chart-line"></i>
+export const transactions = <i className="fa-solid fa-credit-card"></i>
+export const categories = <i className="fa-solid fa-tags"></i>
+export const accounts = <i className="fa-solid fa-wallet"></i>
+export const settings = <i className="fa-solid fa-cog"></i>
+export const logout = <i className="fa-solid fa-sign-out"></i>
+export const trend = <i className="fa-solid fa-money-bill-trend-up"></i>
+export const expenses = <i className="fa-solid fa-money-bill-transfer"></i>
+export const money = <i className="fa-solid fa-money-bill"></i>
+export const piggy = <i className="fa-solid fa-piggy-bank"></i>
+export const yt = <i className="fa-brands fa-youtube"></i>
+export const card = <i className="fa-brands fa-cc-visa"></i>
+export const users = <i className="fa-solid fa-users-between-lines"></i>
+export const dollar = <i className="fa-solid fa-dollar-sign"></i>
+export const calender = <i className="fa-solid fa-calendar"></i>
+export const comment = <i className="fa-solid fa-comment"></i>
+export const plus = <i className="fa-solid fa-plus"></i>
+export const trash = <i className="fa-solid fa-trash"></i>
+export const signout = <i className="fa-solid fa-right-from-bracket"></i>
+export const supply = <i className= "fa-sharp fa-solid fa-boxes-packing" ></i>
+export const medical = <i className="fa-solid fa-briefcase-medical"></i>
+export const circle = <i className="fa-solid fa-circle-dot"></i>
+export const transport=<i className="fa-solid fa-van-shuttle"></i>
+export const formation=<i className="fa-solid fa-symbols"></i>
+export const club=<i className="fa-duotone fa-puzzle"></i>
+export const electricity= <i className="fa-solid fa-bolt"></i>
+export const wifi= <i className="fa-regular fa-wifi"></i>
